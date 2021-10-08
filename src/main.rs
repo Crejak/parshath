06ad7f16bbade3f3c85779b97c53ab87403ce1c7
Valentin Rigolle <valentin.rigolle@gmail.com>
2021-10-09 01:10:59 +0200

    lecture grammaire

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,163 @@
-use std::cmp::PartialOrd;
+use crate::State::{InVar, Neutral};
 
 fn main() {
-    let array = ["d", "gh", "jo", "ye", "nc", "ae"];
-    println!("{:?}", array);
-    let sorted = merge_sort(&array);
-    println!("{:?}", sorted);
+    let grammar = "<Test> ::= \"a\" <Test> | \"\"";
+    let g = get_grammar(grammar);
+    println!("{:?}", g);
 }
 
-fn merge_sort<T: Clone + Copy + PartialOrd>(array: &[T]) -> Vec<T> {
-    let len = array.len();
-    if len == 1 {
-        return Vec::from(array);
-    }
-    if len == 2 {
-        let mut clone = Vec::from(array);
-        if array[1] < array[0] {
-            clone.swap(0, 1);
-        }
-        return clone;
-    }
-    let mid_index = len / 2;
-    let left = merge_sort(&array[0..mid_index]);
-    let right = merge_sort(&array[mid_index..]);
-    let mut result = Vec::with_capacity(len);
-    let mut left_index = 0;
-    let mut right_index = 0;
-    for _ in 0..len {
-        if right_index >= right.len() || left_index < left.len() && left[left_index] <= right[right_index] {
-            result.push(left[left_index]);
-            left_index += 1;
-        } else {
-            result.push(right[right_index]);
-            right_index += 1;
+#[derive(Debug, PartialEq)]
+enum State {
+    Neutral,
+    InVar,
+    InTer
+}
+
+#[derive(Debug)]
+struct Variable {
+    name: String
+}
+
+impl Variable {
+    fn from(name: String) -> Self {
+        Variable {
+            name
         }
     }
-    return result;
+}
+
+#[derive(Debug)]
+enum Terminal {
+    Char(char),
+    Epsilon
+}
+
+#[derive(Debug)]
+enum Symbol {
+    Var(Variable),
+    Ter(Terminal)
+}
+
+impl Symbol {
+    fn var(name: String) -> Self {
+        Symbol::Var(Variable {
+            name
+        })
+    }
+
+    fn ter(char: char) -> Self {
+        Symbol::Ter(Terminal::Char(char))
+    }
+
+    fn eps() -> Self {
+        Symbol::Ter(Terminal::Epsilon)
+    }
+}
+
+#[derive(Debug)]
+struct Rule {
+    left: Variable,
+    right: Vec<Symbol>
+}
+
+impl Rule {
+    fn from(left: String, right: Vec<Symbol>) -> Self {
+        Rule {
+            left: Variable::from(left),
+            right
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Grammar {
+    rules: Vec<Rule>
+}
+
+fn get_grammar(source: &str) -> Option<Grammar> {
+    let mut rules = Vec::new();
+
+    let lines = source.lines();
+
+    for (line_index, line) in lines.enumerate() {
+        let mut left = None;
+        let mut right = Vec::new();
+        let mut current_variable = String::new();
+        let mut state = Neutral;
+        let splits: Vec<&str> = line.split("::=").collect();
+
+        if splits.len() != 2 {
+            panic!("Found {} rule divide (::=) on line {}, expected 1", splits.len(), line_index);
+        }
+
+        // left
+        let left_str = splits[0];
+        for (_char_index, char) in left_str.chars().enumerate() {
+            if state == InVar {
+                if char == '>' {
+                    left = Some(current_variable.clone());
+                    current_variable.clear();
+                    state = Neutral;
+                    break;
+                }
+                current_variable.push(char);
+            } else if char == '<' {
+                state = InVar;
+            }
+        }
+
+        if left == None {
+            panic!("No variable found on left hand sign (line {})", line_index);
+        }
+
+        // right
+        let right_str = splits[1];
+        let mut chars_in_terminal = 0;
+        for (_char_index, char) in right_str.chars().enumerate() {
+            state = match (state, char) {
+                (State::InVar, '>') => {
+                    right.push(Symbol::var(current_variable.clone()));
+                    current_variable.clear();
+                    State::Neutral
+                },
+                (State::InVar, _) => {
+                    current_variable.push(char);
+                    State::InVar
+                },
+                (State::InTer, '"') => {
+                    if chars_in_terminal == 0 {
+                        right.push(Symbol::eps());
+                    } else {
+                        chars_in_terminal = 0;
+                    }
+                    State::Neutral
+                },
+                (State::InTer, _) => {
+                    right.push(Symbol::ter(char));
+                    chars_in_terminal += 1;
+                    State::InTer
+                },
+                (State::Neutral, '|') => {
+                    rules.push(Rule::from(left.clone()?, right));
+                    right = Vec::new();
+                    State::Neutral
+                },
+                (State::Neutral, '"') => {
+                    State::InTer
+                },
+                (State::Neutral, '<') => {
+                    State::InVar
+                },
+                (State::Neutral, _) => {
+                    State::Neutral
+                }
+            };
+        }
+
+        rules.push(Rule::from(left?, right));
+    }
+
+    Some(Grammar {
+        rules
+    })
 }
